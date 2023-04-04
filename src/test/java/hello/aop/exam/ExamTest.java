c5bec50ec82f264ff2baf205ad962b574828cae0
Keunoh <rmsdhcjswo1@gmail.com>
2023-04-04 23:28:24 +0900

    재시도 Aspect

--- src/test/java/hello/aop/exam/ExamTest.java
+++ src/test/java/hello/aop/exam/ExamTest.java
@@ -1,6 +1,8 @@
 package hello.aop.exam;
 
 
+import hello.aop.exam.annotation.Retry;
+import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -9,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
 @SpringBootTest
 class ExamTest {
 
